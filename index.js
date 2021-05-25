f91e91af4ffdc2f0647e99520cdf8865a297938a
sim <cscaesim@gmail.com>
2021-05-25 16:16:14 -0500

    have 1 data set being converted into readings, I'm susipicious of the values converting correctly, but each hex test I try is converting to the same value as an online hex to int converter, but I will double check later

--- index.js
+++ index.js
@@ -31,49 +31,104 @@ connection.connect(function (error) {
 
 connection.query('SELECT trace_id, trace_data FROM test', function(error, results, fields) {
     if (error) throw error;
-    let count = 0;
-    let packet_size = 4;
-    let set = [];
+    
 
     let data = results[0].trace_data
 
-    // let trace_data_size = data.length;
-    let trace_data_size = 5;
-    console.log("lenght", trace_data_size)
+    let graph_set = []
 
-    // console.log("trace_id: ", results[count].trace_id)
-    // console.log("trace_id: ", results[count].trace_data)
-    let step = 1;
-    let packet = [];
+    results.forEach(element => {
+        let data_set = create_data_set(element.trace_data)
+        // console.log("DATASET VALUE: ", data_set)
+    });
 
-    while (count < trace_data_size) {
-        
-        if (step <= packet_size) {
-            // console.log("pushing ", data[count])
-            packet.push(data[count].toString(16))
-            // packet = packet.join('')
-            // console.log(packet.join(''))
-            console.log(packet)
-            // console.log("back to number", parseInt(packet, 32))
-            step++
-        } else {
-            set.push(packet)
-            // console.log("reset step")
-            // console.log("Check: ", set)
-            step = 1;
-            // packet = [];
-        }
-        console.log('packet', packet.join(''))
-        // packet = packet.join('')
-        
-        // console.log("count", count)
-        count++
-    }
+    let data_set_one = create_data_set(data)
+
+    console.log("datasettest", data_set_one)
+
+    run_test(['ff','ff','33','01'])
+    run_test(['c3', 'bf', '23', '2e'])
     // console.log('hextoInt?', parsInt(packet.join(''), 16))
 
     // console.log(set)
 
 })
 
+function create_data_set(data) {
+    let count = 0;
+    let packet = [];
+    let packet_size = 4;
+    let set = [];
+    let step = 1;
+    let trace_data_size = data.length;
+
+    while (count < trace_data_size) {
+        if (step <= packet_size) {
+
+            // console.log("pushing ", data[count])
+
+            packet.push(data[count].toString(16))
+            
+            // packet = packet.join('')
+            // console.log(packet.join(''))
+            // console.log(packet)
+
+            // console.log("back to number", parseInt(packet, 32))
+
+            step++
+        } else {
+            // console.log('packet', packet.join(''))
+
+            let new_packet = convertToHex(packet)
+            let complement = convertHexToSigned(new_packet)
+
+            // console.log("new_int", complement)
+            let reading = convertToReading(complement)
+            // console.log("convert to reading", reading)
+            set.push(reading)
+            step = 1;
+            packet = [];
+        }
+        // console.log("count", count)
+        count++
+        // console.log("set", set)
+    }
+
+    return set;
+}
+
+
+// MARK:- This is a quick function to test my convertions, had to quadruple check to make sure they worked :) 
+function run_test(packet) {
+    let hex = convertToHex(packet)
+    console.log("hexTest", hex)
+
+    let comp = convertHexToSigned(hex)
+    console.log("signed text", comp)
+
+    let reading = convertToReading(comp)
+    console.log("reading test", reading)
+
+
+}
+
 
 // Going to need to get chunks of 4 at a time, convert to int, then get signed 32 bit
+// MARK:- Functions to create our hex values, then convert them to our signed 32bit integer values. And one function to convert to our reading values.
+function convertToHex(packet) {
+    let hex = packet.join('')
+
+    return hex
+}
+
+function convertHexToSigned(hexString) {
+    let value = parseInt(hexString, 16)
+    // This should convert 2's complement but we shall see :)
+    return -(~value + 1)
+}
+
+function convertToReading(value) {
+    return (value / 1000)
+}
+
+
